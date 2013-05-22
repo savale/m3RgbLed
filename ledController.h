887399aeef7745bfdd00502aaa5f4ee717f73074
Sander van Leeuwen <djsavale@hotmail.com>
2013-05-22 10:35:31 +0300

    Update ledController.h
    Next gen idea for controlling the led

--- ledController.h
+++ ledController.h
@@ -1,5 +1,9 @@
 /* Control all leds with higher level funtions */
 
+
+/* sets the maximum update rate for the leds (i.e. for fading) */ 
+#define MIN_TIME_BETWEEN_UPDATES_MS    20 
+
 enum Color{
   None,
   One,
@@ -7,18 +11,48 @@ enum Color{
   Three
 };
 
+enum ActionType
+{
+  Normal,         /* Only uses the imediate color and power */
+  OnNextBeat,     /* Changes from color to nextColor and power to nextPower on the next audio beat */
+  Timed,          /* Changes from color to nextColor and power to nextPower during the set time (morphing) */
+  OnNextBeatTimed /* Changes from color to nextColor and power to nextPower during the set time (morphing) on the next beat */
+}
 
-/* Must wait 
-enum State{
-  Idle,
-  Busy /* busy with wating, fading or waiting for a beat */
+
+/* LED masks */
+#define LED_ALL   0b1111
+
+#define LED_01    0b0001;
+#define LED_02    0b0010;
+#define LED_03    0b0100;
+#define LED_04    0b1000;
+
+
+/* private */
+stuct ledControlState
+{
+  u16 actionTimeMs;                 /* Time the action is already running */
+  bool ledUpdated;                  /* indicates if the led state is update */
 };
 
+
+struct LedControl
+{
+  Color       color;                /* The color the led will have imediately */
+  Color       nextColor;            /* The color the led will have after the action completed */
+  u16         powerPercentage;      /* The power the led will have imediately, 0% is off, 100% is full power */
+  u16         nextPowerPercentage;  /* The power the led will have after the action completed */
+  ActionType  actionType;           /* Type of action */
+  u16         durationAction;       /* duration of the action in case of a timed action (miliseconds) */
+};
+
+
 /* Set led brightness: 100% is full, 0% = off */
-void setAllLedPower(u16 percentage);
+void setLedControl(ledControl &ledControl, u64 &ledMask);
 
-void setAllLeds(Color color);
 
+/* Private */
 /* Poll the state of an action */
 State getState(void);
 
@@ -26,11 +60,11 @@ State getState(void);
 
 /* Actions (only one at a time, must poll the state first) */
 
-void setAllFadeOut(u16 timeMs);
+/* Keeps the state busy for a specified time */
+void waitTime(u16 timeMs);
 
-void setAllFadeIn(u16 timeMs);
+/* Keeps the state to busy while not detecting a "beat" in the audio */
+void waitForNextBeat(void);
 
-void wait(u16 timeMs);
-
-void waitForBeat(void);
-
+/* Keeps the state to busy while not detecting a "beat" in the audio */
+void waitForAllCompleted(void);
